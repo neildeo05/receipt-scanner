6767988c7e7241291bc357e513dd7bd98be7a737
neildeo05 <30543077+neildeo05@users.noreply.github.com>
2021-02-26 13:02:09 -0800

    new code

--- src/text.py
+++ src/text.py
@@ -6,16 +6,16 @@ import matplotlib.pyplot as plt
 import sys
 
 filename = sys.argv[1]
-img = cv2.imread(filename)
-d = pytesseract.image_to_data(img, output_type=Output.DICT)
+original_img = cv2.imread(filename)
+d = pytesseract.image_to_data(original_img, output_type=Output.DICT)
+img = original_img.copy()
 for i in range(len(d['level'])):
     (x,y,w,h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
     img = cv2.rectangle(img, (x,y), (x + w, y + h), (255, 0, 0), 2)
 
+plt.imshow(original_img)
 
-plt.imshow(img);
-
-print(pytesseract.image_to_string(img, config='--psm 6'))
+print(pytesseract.image_to_string(original_img, config='--psm 6'))
 
 
 plt.show()
